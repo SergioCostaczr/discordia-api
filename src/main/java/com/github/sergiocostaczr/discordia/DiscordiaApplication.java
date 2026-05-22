06d60efd710aee5cf97c0b424a477d01e984e3db
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-05-22 18:33:56 -0300

    first commit

---
+++ src/main/java/com/github/sergiocostaczr/discordia/DiscordiaApplication.java
@@ -0,0 +1,13 @@
+package com.github.sergiocostaczr.discordia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordiaApplication.class, args);
+	}
+
+}
