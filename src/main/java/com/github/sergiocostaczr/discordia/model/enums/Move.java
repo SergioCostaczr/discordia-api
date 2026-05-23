dd0edafa8744a107a3e23e8370f6e173fd1e23eb
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-05-23 02:23:47 -0300

    feat: adiciona enums Move, Role e RoundStatus

---
+++ src/main/java/com/github/sergiocostaczr/discordia/model/enums/Move.java
@@ -0,0 +1,7 @@
+package com.github.sergiocostaczr.discordia.model.enums;
+
+public enum Move {
+    ROCK,
+    PAPER,
+    SCISSORS,
+}
---
+++ src/main/java/com/github/sergiocostaczr/discordia/model/enums/RoundStatus.java
@@ -0,0 +1,8 @@
+package com.github.sergiocostaczr.discordia.model.enums;
+
+public enum RoundStatus {
+    WAITING,
+    IN_PROGRESS,
+    FINISHED,
+    CANCELLED
+}
